9b6f5198f35a60c0c4b9e349fb044abdb897feaf
HeyArtem <artem_white@mail.ru>
2020-12-01 01:38:13 +0300

    Лекция 11.1 Базовые магические методы: __init__, __str__, __del__, __getitem__, __len__. Так же дописываю части для других страниц. Статические методы и методы класса

--- Class_point2D.py
+++ Class_point2D.py
@@ -84,6 +84,37 @@ class Point2D():
 
 
 
+    # Часть 4 -----Статические методы и методы класса------------
+    '''' Статический метод '''
+    @staticmethod  # забронированно слово-декоратор
+    def stat_method_ex(x, y):  # это статическая функция данного класса
+        if(x**2 + y**2)**0.5 > 1:
+            return 1
+        return 0
+
+    ''' Метод класса '''
+    @classmethod
+    def class_method_ex(cls):
+        ''' Сделаем что бы возвращался лист из объектов класса '''
+        point_list = []
+        for i in range(10):
+            point_list.append(cls(i, i + 1))
+        return point_list
+    ''''
+    Стат методы и методы класса исполбзуются реже, чем методы
+    класса.
+    
+    На вход стат методу подаются некоторые парамтры и не подаются не объекты не классы
+    Методы класса обычно используют для генерации каких то спец. объектов
+    На вход подаётся сам класс'''
+
+
+
+
+
+
+
+
 
 
 
@@ -92,22 +123,40 @@ class Point2D():
 
 
 if __name__ == '__main__':
-    point1 = Point2D(1,1)
-    print('Работа метода __str__: ',point1)
-    # del point1 # закоментил,т.к дальше print не будет работать
-    str1 = 'volvo'
-    list_test = [1,2,3,4]
-    print('Работа метода __len__ (в 3-ем Len):',len(str1), len(list_test), len(point1))
-    print('Метод dictance:',point1.distance())
+    # point1 = Point2D(1,1)
+    # print('Работа метода __str__: ',point1)
+    # # del point1 # закоментил,т.к дальше print не будет работать
+    # str1 = 'volvo'
+    # list_test = [1,2,3,4]
+    # print('Работа метода __len__ (в 3-ем Len):',len(str1), len(list_test), len(point1))
+    # print('Метод dictance:',point1.distance())
+    #
+    # for item in str1:
+    #     print('Проэтирировали volvo:',item)
+    #
+    # for item in list_test:
+    #     print('Проэтирировали list_test:',item)
+    #
+    # print('Вычислим, находятся ли точки в диапазоне:\n',2 in point1, 1 in point1) # это мы тестим getitem
+    #
+    # for coord in point1:
+    #     print(coord)
 
-    for item in str1:
-        print('Проэтирировали volvo:',item)
 
-    for item in list_test:
-        print('Проэтирировали list_test:',item)
 
-    print('Вычислим, находятся ли точки в диапазоне:\n',2 in point1, 1 in point1) # это мы тестим getitem
+# Предыдущее закоментил, т.к. дальше Стат метод и метод класса
 
-    for coord in point1:
-        print(coord)
+    list_ = Point2D.class_method_ex()
+    ''' т.к. это метод класса, то он вызывается через класс точка и данный метод'''
+    print(' не красиво, str здесь не работатет:',list_)
+    '''' print(list_) - выведет некрасиво'''
+    for i in range(10):
+        '''' Запринтим каждый элемент листа'''
+        print(list_[i])
 
+    print(Point2D.stat_method_ex(1, 2))
+    ''''
+    Работаем со стат методом. 
+    У вектора (1, 2) расстояние > 1
+    поэтому должен вернуть 1
+    '''
