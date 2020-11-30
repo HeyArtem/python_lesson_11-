f14b4f5bb38d796d539e8c78c93f0e44d3e1906b
HeyArtem <artem_white@mail.ru>
2020-11-30 16:38:32 +0300

    Лекция 11.1 Базовые магические методы: __init__, __str__, __del__, __getitem__, __len__

--- Class_point2D.py
+++ Class_point2D.py
@@ -5,15 +5,16 @@
 !!! Почему так велется лекция, почему преподователь не гтовится заранее, а на средине лекции
 меняеются начальные условия и потом идут исправления по всему коду
 
-Я исправил, но у меня код выполняется с ошибкой. Гед она?
+
 
 '''
 
 
 
-
+# Часть 1 - т.е для лекции 11.1
 class Point2D():
 
+    # Часть 1
     def __init__(self, x, y):
         self.coord = [x, y]
 
@@ -37,6 +38,38 @@ class Point2D():
         '''' Аналог len'''
         return (self.coord[0]**2 + self.coord[1]**2)**0.5
 
+    # Часть 2---------------------------------------------
+
+    def __eq__(self, other):
+        ''''Напишкем магич.метод котор будет работать при сравнении'''
+        return (self.coord[0] == other.coord[0]) & (self.coord[1]== other.coord[1])
+
+    def __lt__(self, other):
+        ''''
+        метод сравнения, на уроке не не скажут,что этот метод подразузомивает,
+        что левая част неравенства, должна быть меньше правой, в этом случае результат True.
+        Есть противополождые методы
+        '''
+        return (self.distance() < other.distance())
+
+    def __add__(self, other):
+        '''' if isinstance(other) отвечает за тип объекта'''
+        if isinstance(other, Point2D): # это мы дописыввем, что бы можно было выполнить point1+1
+            return Point2D(self.coord[0] + other.coord[0], self.coord[1] + other.coord[1])
+        if isinstance(other, int):
+            return Point2D(self.coord[0] + other, self.coord[1] + other)
+
+    def __float__(self): # Point2D -> float
+        return self.distance()
+
+
+
+
+
+
+
+
+    # Часть 2---------------------------------------------
 if __name__ == '__main__':
     point1 = Point2D(1,1)
     print('Работа метода __str__: ',point1)
@@ -54,5 +87,6 @@ if __name__ == '__main__':
 
     print('Вычислим, находятся ли точки в диапазоне:\n',2 in point1, 1 in point1) # это мы тестим getitem
 
-    # for coord in point1:
-    #     print(coord)
+    for coord in point1:
+        print(coord)
+
