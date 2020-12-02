cf7501841138cd965d31ddf8ba9cba34f7c9c6e8
HeyArtem <artem_white@mail.ru>
2020-12-02 20:47:58 +0300

    Домашнее задание по главе 11. Здесь находится class Room, на который я буду писать магические методы.

--- hw_class_Room.py
+++ hw_class_Room.py
@@ -11,15 +11,14 @@ class Room(): # Простая модель комнаты
     отвечает за инициализацию
     '''
 
-    def __init__(self, name, length, width, height):
+    def __init__(self, name, length, width):
         '''
          Инициализируем атрибуты
-         Длина, Ширина, Высота
+         Длина, Ширина
         '''
         print('    Старт метод Метод 1 __init__.')
         self.length = length
         self.width = width
-        self.height = height
         self.name = name
 
     def __str__(self):
@@ -28,9 +27,7 @@ class Room(): # Простая модель комнаты
         ОТвечает за отображение объекта,
         т.е. если к нему применить print
         '''
-
-        # print('    Старт метод Метод 2 __str__.')
-        return f"Помещение:{self.name} "
+        return f"Помещение: {self.name} "
 
     def __repr__(self):
         '''
@@ -38,8 +35,6 @@ class Room(): # Простая модель комнаты
         как он будет оттображаться в системе,
         т.е. как его будут видеть разработчики.
         '''
-
-        # print('    Старт метод Метод 3 __repr__.')
         return f" Объект имя: {self.name}"
 
     def __add__(self, other_length):
@@ -49,8 +44,6 @@ class Room(): # Простая модель комнаты
         Предположим после перепланировки параметр
         lenght (длина) увеличится.
         '''
-
-        # print('    Старт метод Метод 4 __add__. Увеличиваем длину')
         if isinstance(other_length, (int, float)):
             return self.length + other_length
 
@@ -61,10 +54,9 @@ class Room(): # Простая модель комнаты
         но в случаях, когда главный объект находится
         СПРАВА
         '''
-
-        # print('    Старт метод Метод 5 __radd__. Увеличиваем длину, объект СПРАВА')
         if isinstance(other, (int, float)):
             return self + other
+
 
     def __eq__(self, other):
         '''
@@ -75,10 +67,9 @@ class Room(): # Простая модель комнаты
         True-> если величины равны
         False-> если величины отличаются
         '''
-
-        # print('    Старт метод Метод 6 __eq__.')
         return isinstance(other, Room) and \
                self.length == other.length and self.width == other.width
+
 
     def __bool__(self):
         '''
@@ -86,55 +77,43 @@ class Room(): # Простая модель комнаты
         Если хотя бы один параметр в габаритах комнаты
         будет равен ноль (помещение не реальное), метод выведет False
         '''
-        # print('    Старт метод Метод 7 __bool__.')
-        return (self.length != 0 and self.width != 0 and self.height != 0)
+        return (self.length != 0 and self.width != 0)
+
+    def __mul__(self):
+        ''''
+        Метод 8 __mul__ Умножение
+        Я считаю площадь помещения
+        перемножая length(длину) & width (ширину)
+        '''
+        return (self.length * self.width)
 
 
+    def __sub__(self, other):
+        ''''
+        Метод 9 __sub__ Вычитание
+        Здесь считаю разницу в площадях
+        между двумя помещениями
+        '''
+        return f"Метод 9 __sub__ Разница площадей у помещений: {abs((self.length * self.width) - (other.length * other.width))} кв.м"
 
 
-    # !?!?!Эльдар, не могу составить код, что бы прибавлялись все три габарита в одной строке
-    # def __add__(self, other_length, other_width, other_height):
-    #     '''
-    #     Метод 4 __add__
-    #     Предположим после перепланировки какой то из
-    #     параметров (габаритов) комнаты увеличился.
-    #     '''
-    #     print('    Старт метод Метод 4 __add__')
-    #     if isinstance(other, (int, float)):
-    #         return self.length + other_length, self.width + other_width,  self.height + other_height
+    def __format__(self):
+        ''''
+        Метод 10 форматное представление
+        '''
+        return f"Метод 10. Помещение: {self.name}, Длина:{self.length}м. Ширина:{self.width}м. Общая площадь:{self.length*self.width}кв.м"
 
-
-
-
-    # def __len__(self):  # что то пока не так
-    #     '''
-    #     Метод 7  __len__
-    #
-    #     '''
-    #     print('    Старт метод Метод 7 __len__.')
-    #     # res = float(abs(self.length - self.width))
-    #     # print(res)
-    #     # if res > 0 or res < 0:
-    #     #     return 'Помещение прямоугольное'
-    #     # return 'Помещение квадратное'
-    #     return len(self.name)
-
-
-
-
-    def __dir__(self):
-        # print('  НЕ ИДЕТ  Старт метод Метод 9 __dir__.')
-        # return self.name
-        return
-
-    # def __new__(cls, *args, **kwargs):
-    #     return
 
 
 
 
 if __name__ == '__main__':
-    kitchen_room = Room('кухня', 20, 3, 3)
+    kitchen_room = Room('Kухня', 1, 2)
+    kitchen_room_2 = Room('Кухня_2', 10, 2)
+
+    print(kitchen_room.__sub__(kitchen_room_2))
+
+    print(kitchen_room.__format__())
 
 
 
@@ -142,22 +121,3 @@ if __name__ == '__main__':
 
 
 
-
-    # print('lenLen', len.kitchen_room)
-
-
-
-    #print(bool(kitchen_room))
-
-
-
-
-    print(kitchen_room.__dir__())
-
-
-
-
-
-    # new_lenght = kitchen_room.length+10
-    # print(new_lenght)
-    # new_lenght_2 = kitchen_room+10
